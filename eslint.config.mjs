cd3dd6a16aaed53c4664f9f1eecf584749950b5a
Fathismael93 <fathismael@gmail.com>
2025-03-16 00:56:36 -0800

    First commit for initializing in Github

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +12,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals'),
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
 
 export default eslintConfig;
---
+++ middleware.js
@@ -0,0 +1,39 @@
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  async function middleware(req) {
+    // authorize roles
+    const url = req.nextUrl.pathname;
+    const user = req?.nextauth?.token?.user;
+
+    if (url.startsWith('/api')) {
+      NextResponse.next().headers.append('Access-Control-Allow-Origin', '*');
+    }
+
+    if (
+      url?.startsWith('/me') ||
+      url?.startsWith('/address') ||
+      url?.startsWith('/cart') ||
+      url?.startsWith('/shipping')
+    ) {
+      if (!user) {
+        return NextResponse.redirect(new URL('/', req.url));
+      }
+      return NextResponse.next();
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        if (!token) {
+          return false;
+        }
+      },
+    },
+  },
+);
+
+export const config = {
+  matcher: ['/me/:path*', '/address/:path*', '/cart', '/shipping'],
+};
