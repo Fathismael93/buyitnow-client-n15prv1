2b93dfac7003dd4e7bc392e7b60707bc5a4128e3
Fathismael93 <fathismael@gmail.com>
2025-03-20 00:05:42 -0800

    handling issue

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -5,24 +5,31 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req) {
   try {
+    console.log('We are in the get products get request');
     const resPerPage = 2;
 
+    console.log('GET API FILTER AND INVOKE SEARCH AND FILTER');
     const apiFilters = new APIFilters(Product.find(), req.query)
       .search()
       .filter();
 
+    console.log('GET PRODUCTS FROM API FILTER AFTER SEARCH AND FILTER');
     let products = await apiFilters.query.populate('category');
     const filteredProductsCount = products.length;
 
+    console.log('INVOKE PAGINATION FROM API FILTER');
     apiFilters.pagination(resPerPage);
 
+    console.log('GET PRODUCTS FROM API FILTER AFTER PAGINATION');
     products = await apiFilters.query.populate('category').clone();
 
     const result = filteredProductsCount / resPerPage;
     const totalPages = Number.isInteger(result) ? result : Math.ceil(result);
 
+    console.log('GET CATEGORIES FROM DATABASE');
     const categories = await Category.find();
 
+    console.log('RETURN RESPONSE');
     return NextResponse.json(
       {
         success: true,
