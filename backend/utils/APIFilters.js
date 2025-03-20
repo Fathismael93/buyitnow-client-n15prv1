8ddd0318b1df851d3affe7be84f7d286ff88761f
Fathismael93 <fathismael@gmail.com>
2025-03-20 11:25:50 -0800

    Checking queryStr in apiFilter

--- backend/utils/APIFilters.js
+++ backend/utils/APIFilters.js
@@ -18,9 +18,9 @@ class APIFilters {
       : {};
 
     console.log('are we coming to the end of the search method');
-    console.log(keyword);
+    console.log({ ...keyword });
 
-    this.query = this.query.find({ ...keyword });
+    this.query.find({ ...keyword });
     console.log('After the search, returning this.query');
     console.log(this.query);
     return this;
