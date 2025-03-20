8f2ee53c2aba0b4231197cfdf60eebe340f05f4b
Fathismael93 <fathismael@gmail.com>
2025-03-20 00:12:13 -0800

    implement api router and deleted pages/api for products api

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,5 @@
 import Product from '@/backend/models/product';
-import Category from '@/backend/models/category';
+// import Category from '@/backend/models/category';
 import APIFilters from '@/backend/utils/APIFilters';
 import { NextResponse } from 'next/server';
 
@@ -9,34 +9,42 @@ export async function GET(req) {
     const resPerPage = 2;
 
     console.log('GET API FILTER AND INVOKE SEARCH AND FILTER');
-    const apiFilters = new APIFilters(Product.find(), req.query)
+    new APIFilters(Product.find(), req.query)
       .search()
-      .filter();
+      .filter()
+      .then((result) => {
+        console.log('RESULT FROM API FILTER');
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log('ERROR FROM API FILTER');
+        console.log(err);
+      });
 
-    console.log('GET PRODUCTS FROM API FILTER AFTER SEARCH AND FILTER');
-    let products = await apiFilters.query.populate('category');
-    const filteredProductsCount = products.length;
+    // console.log('GET PRODUCTS FROM API FILTER AFTER SEARCH AND FILTER');
+    // let products = await apiFilters.query.populate('category');
+    // const filteredProductsCount = products.length;
 
-    console.log('INVOKE PAGINATION FROM API FILTER');
-    apiFilters.pagination(resPerPage);
+    // console.log('INVOKE PAGINATION FROM API FILTER');
+    // apiFilters.pagination(resPerPage);
 
-    console.log('GET PRODUCTS FROM API FILTER AFTER PAGINATION');
-    products = await apiFilters.query.populate('category').clone();
+    // console.log('GET PRODUCTS FROM API FILTER AFTER PAGINATION');
+    // products = await apiFilters.query.populate('category').clone();
 
-    const result = filteredProductsCount / resPerPage;
-    const totalPages = Number.isInteger(result) ? result : Math.ceil(result);
+    // const result = filteredProductsCount / resPerPage;
+    // const totalPages = Number.isInteger(result) ? result : Math.ceil(result);
 
-    console.log('GET CATEGORIES FROM DATABASE');
-    const categories = await Category.find();
+    // console.log('GET CATEGORIES FROM DATABASE');
+    // const categories = await Category.find();
 
     console.log('RETURN RESPONSE');
     return NextResponse.json(
       {
         success: true,
         data: {
-          categories,
-          totalPages,
-          products,
+          categories: [],
+          totalPages: resPerPage,
+          products: [],
         },
       },
       { status: 200 },
