e316cf22d51d8d4c3787c40b06bd1cf3c50942a3
Fathismael93 <fathismael@gmail.com>
2025-03-20 00:18:57 -0800

    handling issue

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -6,7 +6,14 @@ const dbConnect = () => {
   }
 
   mongoose.set('strictQuery', false);
-  mongoose.connect(process.env.DB_URI);
+  mongoose
+    .connect(process.env.DB_URI)
+    .then((result) => {
+      console.log('CONNECTED TO DATABASE');
+    })
+    .catch((err) => {
+      console.log('ERROR IN CONNECTING TO THE DATABASE');
+    });
 };
 
 export default dbConnect;
