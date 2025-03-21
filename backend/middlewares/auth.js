dd84d7c756d6264dab40469bd9bf0dd3dd907fab
Fathismael93 <fathismael@gmail.com>
2025-03-21 12:02:14 -0800

    Implemented api cart and deleted pages/api/cart/index.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,11 +3,17 @@ import { getServerSession } from 'next-auth';
 import auth from '@/pages/api/auth/[...nextauth]';
 
 const isAuthenticatedUser = async (req, res) => {
+  console.log('WE ARE STARTING TO CHECK SESSION');
   const session = await getServerSession(req, res, auth);
 
+  console.log('WE HAVE GOT SESSION FROM NEXTAUTH');
+
   if (!session) {
+    console.log('THIS SESSION DOES NOT EXIST');
     return new ErrorHandler('Login first to access this route', 401);
   }
+
+  console.log('THIS SESSION DOES EXIST');
 
   req.user = session.user;
 
