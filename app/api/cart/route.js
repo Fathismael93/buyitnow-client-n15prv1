ed2f72636e3de38cea8e1205157c1bf50090c2c4
Fathismael93 <fathismael@gmail.com>
2025-03-21 14:27:08 -0800

    Implemented post item in cart

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -60,3 +60,59 @@ export async function GET(req) {
     );
   }
 }
+
+export async function POST(req) {
+  try {
+    await isAuthenticatedUser(req, NextResponse);
+
+    dbConnect();
+
+    const user = await User.findOne({ email: req.user.email }).select('_id');
+
+    if (!user) {
+      return NextResponse.next(new ErrorHandler('User not found', 404));
+    }
+
+    const body = JSON.parse(req.body);
+
+    const product = await Product.findById(body.productId);
+
+    if (!product) {
+      return NextResponse.next(new ErrorHandler('Product not found', 404));
+    }
+
+    let quantity = 1;
+
+    // IF QUANTITY ASKED BY THE USER IS MORE THEN THE PRODUCT'STOCK...
+
+    if (quantity > product.stock) {
+      return NextResponse.next(new ErrorHandler('Product inavailable', 404));
+    }
+
+    const cart = {
+      product: product._id,
+      user: user._id,
+      quantity,
+    };
+
+    const cartAdded = await Cart.create(cart);
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: {
+          cartAdded,
+        },
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error,
+      },
+      { status: 500 },
+    );
+  }
+}
