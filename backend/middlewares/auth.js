fd9a45ae79303ce749ecab117815fbe2741c9d07
Fathismael93 <fathismael@gmail.com>
2025-03-24 02:20:50 -0800

    Updated jsconfig.json file

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,5 @@
-import { getServerSession } from 'next-auth';
 import { auth } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 
 const isAuthenticatedUser = async (req, res) => {
   const session = await getServerSession(auth);
